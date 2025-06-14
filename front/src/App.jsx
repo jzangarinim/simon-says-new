5c03456553eacbc83b09594295438bb8f3cd9543
Jose Zangarini <jzangarinim@gmail.com>
2025-06-14 20:18:42 +0200

    generates first game order on page load

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,4 +1,11 @@
+import { useState, useEffect } from "react";
+
+const colorIds = ["green", "red", "yellow", "blue"];
+const startNumber = 4;
+
 function App() {
+  const [order, setOrder] = useState([]);
+
   const colors = [
     { id: "green", className: "bg-green-400 top-0 left-0 rounded-tl-full" },
     { id: "red", className: "bg-red-400 top-0 right-0 rounded-tr-full" },
@@ -8,6 +15,20 @@ function App() {
     },
     { id: "blue", className: "bg-blue-400 bottom-0 right-0 rounded-br-full" },
   ];
+
+  useEffect(() => {
+    const generateInitialOrder = () => {
+      const newOrder = [];
+      for (let i = 0; i < startNumber; i++) {
+        const randomIndex = Math.floor(Math.random() * colorIds.length);
+        newOrder.push(colorIds[randomIndex]);
+      }
+      setOrder(newOrder);
+    };
+
+    generateInitialOrder();
+  }, []);
+
   return (
     <>
       <div className="relative w-96 h-96 mx-auto">
@@ -20,7 +41,12 @@ function App() {
             }}
           ></button>
         ))}
-        <button className="absolute top-1/2 left-1/2 w-24 h-24 bg-gray-800 rounded-full border-4 border-white transform -translate-x-1/2 -translate-y-1/2 cursor-pointer text-white">
+        <button
+          className="absolute top-1/2 left-1/2 w-24 h-24 bg-gray-800 rounded-full border-4 border-white transform -translate-x-1/2 -translate-y-1/2 cursor-pointer text-white"
+          onClick={() => {
+            console.log(`This is supposed to be how you start the game`);
+          }}
+        >
           Start
         </button>
       </div>
